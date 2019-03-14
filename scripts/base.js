879f648d4975e4d269804ebe685888f6a1e3948f
Danielle Parkinson <dparkinson2@hotmail.co.uk>
2019-03-14 17:12:20 +0000

    Set up page layouts, added nav close on page click

--- scripts/base.js
+++ scripts/base.js
@@ -6,15 +6,35 @@
   var navButton = $('nav-button')
   var navPanel = $('nav-panel')
   var menuIcon = $('nav-icon')
+  var iconMenuLabel = $('menu-label')
+  var iconHideLabel = $('hide-label')
+  var pageContent = $('page-content')
 
   if (navButton) {
     navButton.addEventListener('click', function() {
       if (navPanel.classList.contains('show')) {
         navPanel.classList.remove('show')
         menuIcon.classList.remove('open')
+        iconMenuLabel.classList.add('show')
+        iconHideLabel.classList.remove('show')
       } else {
         navPanel.classList.add('show')
         menuIcon.classList.add('open')
+        iconMenuLabel.classList.remove('show')
+        iconHideLabel.classList.add('show')
+      }
+    })
+  } else {
+  }
+
+  if (pageContent) {
+    pageContent.addEventListener('click', function() {
+      if (navPanel.classList.contains('show')) {
+        navPanel.classList.remove('show')
+        menuIcon.classList.remove('open')
+        iconMenuLabel.classList.add('show')
+        iconHideLabel.classList.remove('show')
+      } else {
       }
     })
   } else {
