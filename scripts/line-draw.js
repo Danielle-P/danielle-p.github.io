dedb0f9165fd05da968efd0e84258b3ccfd2023f
Danielle Parkinson <dparkinson2@hotmail.co.uk>
2019-04-06 14:49:45 +0100

    Add javascript for svg line drawing,add svg curves

--- scripts/line-draw.js
+++ scripts/line-draw.js
@@ -3,36 +3,80 @@ function SVGLineDraw() {
     return Array.from(document.getElementsByClassName(className))
   }
 
+  //var svgElements = getElements('svg')
   var journeyMarkers = getElements('journey-marker')
+  var panels = getElements('panel')
 
-  var svgElements = ['line-1'].map(function(id) {
+  var svgElements = ['line-1', 'line-2', 'line-3'].map(function(id) {
     var svgElement = document.getElementById(id)
     return svgElement
   })
 
+  console.log(svgElements)
+
+  var panelHeights = panels.map(function(element) {
+    var panelHeight = element.clientHeight
+    return panelHeight
+  })
+  console.log('panel heights', panelHeights)
+
   var markerXPositions = journeyMarkers.map(function(element) {
     var markerPosition = element.getBoundingClientRect()
     var markerXPosition = markerPosition.x
-    console.log(markerPosition)
     return markerXPosition
   })
+  console.log(markerXPositions)
 
   var markerYPositions = journeyMarkers.map(function(element) {
     var markerPosition = element.getBoundingClientRect()
     var markerYPosition = markerPosition.y
-    console.log(markerPosition)
     return markerYPosition
   })
 
-  svgElements.forEach(function(element, index) {
-    var margin = 48
-    var cPathValues = ' c0 50 0 40 100 40 '
-    var mPathValues = 'M' + (markerXPositions[index] - margin) + ' 0'
-    var vPathValues =
-      'v' + (markerYPositions[index + 1] - markerYPositions[index] - 60)
-    var hPathValues = 'h ' + (markerXPositions[index + 1] - 92 - margin)
-    var drawPath = mPathValues + vPathValues + cPathValues + hPathValues
-    element.setAttribute('d', drawPath)
+  svgElements.forEach(function(element, index, arr) {
+    if (element.classList.contains('odd')) {
+      var margin = 48
+      var cPathValues = ' c0 50 0 40 100 40 '
+      var mPathValues = 'M' + (markerXPositions[index] - margin) + ' 0 '
+      var vPathValues = 'v' + (panelHeights[index] - 60)
+
+      var hPathValues =
+        index === arr.length - 1
+          ? ''
+          : 'h' +
+            (markerXPositions[index + 1] -
+              markerXPositions[index] -
+              margin * 2 -
+              30)
+
+      var c2PathValues = index === arr.length - 1 ? '' : ' c50 0 0 60 40 40 '
+
+      var drawPath =
+        mPathValues + vPathValues + cPathValues + hPathValues + c2PathValues
+
+      element.setAttribute('d', drawPath)
+    } else {
+      var margin = 48
+      var cPathValues = ' c0 50 0 40 -100 40 '
+      var mPathValues = 'M' + (markerXPositions[index] - margin) + ' 0 '
+      var vPathValues = 'v' + (panelHeights[index] - 60)
+
+      var hPathValues =
+        index === arr.length - 1
+          ? ''
+          : 'h' +
+            (markerXPositions[index + 1] -
+              markerXPositions[index] -
+              margin * 2 -
+              30)
+
+      var c2PathValues = index === arr.length - 1 ? '' : ' c50 0 0 60 40 40 '
+
+      var drawPath =
+        mPathValues + vPathValues + cPathValues + hPathValues + c2PathValues
+
+      element.setAttribute('d', drawPath)
+    }
   })
 }
 
