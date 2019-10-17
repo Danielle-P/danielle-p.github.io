8ecbced01e4a513c53e2c47d972388d676a9e6e0
Danielle Parkinson <dparkinson2@hotmail.co.uk>
2019-10-17 21:52:06 +0100

    Add styling and functionality for timeline animation

---
+++ scripts/timeline-animation.js
@@ -0,0 +1,19 @@
+if (document.querySelector('#projects')) {
+  var journeyTopPosition = document
+    .querySelector('.journey')
+    .getBoundingClientRect().top
+  var halfViewportHeight = window.innerHeight * 0.75
+  var markers = document.getElementsByClassName('marker')
+
+  window.addEventListener('scroll', function triggerAnimation(e) {
+    var journeyInView =
+      window.pageYOffset >= journeyTopPosition - halfViewportHeight
+
+    if (journeyInView) {
+      Array.from(markers).forEach(marker => {
+        marker.classList.add('visible')
+      })
+      window.removeEventListener('scroll', triggerAnimation)
+    }
+  })
+}
