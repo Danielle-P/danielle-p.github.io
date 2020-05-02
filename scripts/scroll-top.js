ed410f24fbf435b35b0b98722be1acbd56b4294a
Danielle Parkinson <dparkinson2@hotmail.co.uk>
2020-05-02 16:59:52 +0100

    Various styling tweaks

--- scripts/scroll-top.js
+++ scripts/scroll-top.js
@@ -1,19 +1,21 @@
 var scrollButton = document.getElementById('scroll-top-link')
 
-window.addEventListener('scroll', function() {
-  var scrollTop = window.scrollY
+if (scrollButton) {
+  window.addEventListener('scroll', function () {
+    var scrollTop = window.scrollY
 
-  if (scrollTop > 200) {
-    scrollButton.classList.add('visible')
-  } else {
-    scrollButton.classList.remove('visible')
-  }
-})
-
-scrollButton.addEventListener('click', scrollToTop)
-
-function scrollToTop() {
-  window.scroll({
-    top: 0,
+    if (scrollTop > 200) {
+      scrollButton.classList.add('visible')
+    } else {
+      scrollButton.classList.remove('visible')
+    }
   })
+
+  scrollButton.addEventListener('click', scrollToTop)
+
+  function scrollToTop() {
+    window.scroll({
+      top: 0,
+    })
+  }
 }
