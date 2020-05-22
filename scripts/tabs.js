8683c803bf9a39ee4cfb9b80a6f8da31e5b45501
Danielle Parkinson <dparkinson2@hotmail.co.uk>
2020-05-22 18:44:28 +0100

    Add next project link and functionality to tabs

--- scripts/tabs.js
+++ scripts/tabs.js
@@ -1,27 +1,54 @@
-;(function() {
-  function getElements(className) {
-    return Array.from(document.getElementsByClassName(className))
+;(function () {
+  const getElements = (className) =>
+    Array.from(document.getElementsByClassName(className))
+
+  function switchToSelectedTab({selectedTabClassList, tabContentClassList}) {
+    const slideDownPanels = getElements('slide-down-panel')
+
+    if (!selectedTabClassList.contains('selected')) {
+      document.querySelector('.tab.selected').classList.remove('selected')
+
+      slideDownPanels.forEach((panel) => {
+        if (panel.classList.contains(tabContentClassList)) {
+          panel.classList.toggle('show')
+        } else {
+          panel.classList.remove('show')
+        }
+      })
+      selectedTabClassList.add('selected')
+    }
   }
 
-  var tabsList = getElements('tab')
-  var slideDownPanels = getElements('slide-down-panel')
+  const tabsList = getElements('tab')
+  const nextTabLinks = getElements('next-tab')
 
-  tabsList.forEach(tab => {
-    tab.addEventListener('click', function() {
-      var tabId = this.id
+  tabsList.forEach((tab) => {
+    tab.addEventListener('click', (event) => {
+      const selectedTab = event.currentTarget
 
-      if (!this.classList.contains('selected')) {
-        document.querySelector('.tab.selected').classList.remove('selected')
+      switchToSelectedTab({
+        selectedTabClassList: selectedTab.classList,
+        tabContentClassList: selectedTab.querySelector('a').classList,
+      })
+    })
+  })
 
-        slideDownPanels.forEach(panel => {
-          if (panel.classList.contains(tabId)) {
-            panel.classList.toggle('show')
-          } else {
-            panel.classList.remove('show')
-          }
-        })
-        tab.classList.add('selected')
-      }
+  nextTabLinks.forEach((link) => {
+    link.addEventListener('click', (event) => {
+      const nextTabId = event.currentTarget.id
+
+      tabsList.forEach((tab) => {
+        const nextTabContent = tab.querySelector('a')
+
+        if (nextTabContent.classList.contains(nextTabId)) {
+          const nextTabName = nextTabContent.parentNode.classList
+
+          switchToSelectedTab({
+            selectedTabClassList: nextTabName,
+            tabContentClassList: nextTabContent.classList,
+          })
+        }
+      })
     })
   })
 })()
