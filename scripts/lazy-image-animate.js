b62830442b016507c93cff26638c1051dbcb2df0
Danielle Parkinson <dparkinson2@hotmail.co.uk>
2020-05-27 21:53:43 +0100

    Add lazy loading images

--- scripts/lazy-image-animate.js
+++ scripts/lazy-image-animate.js
@@ -1,16 +1,17 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   var lazyImageSections = Array.from(
     document.querySelectorAll('.image-intersect')
   )
 
   if ('IntersectionObserver' in window) {
     var lazyImageObserver = new IntersectionObserver(
-      function(imageSections) {
-        imageSections.forEach(function(imageSection) {
+      function (imageSections) {
+        imageSections.forEach(function (imageSection) {
           if (imageSection.isIntersecting) {
             Array.from(
               imageSection.target.querySelectorAll('img.lazy')
-            ).forEach(function(lazyImage) {
+            ).forEach(function (lazyImage) {
+              lazyImage.src = lazyImage.dataset.src
               lazyImage.classList.remove('lazy')
               lazyImageObserver.unobserve(lazyImage)
             })
@@ -21,11 +22,11 @@ document.addEventListener('DOMContentLoaded', function() {
         threshold: 0.6,
       }
     )
-    lazyImageSections.forEach(function(lazyImageSection) {
+    lazyImageSections.forEach(function (lazyImageSection) {
       lazyImageObserver.observe(lazyImageSection)
     })
   } else {
-    Array.from(document.querySelectorAll('img.lazy')).forEach(function(
+    Array.from(document.querySelectorAll('img.lazy')).forEach(function (
       lazyImage
     ) {
       lazyImage.classList.remove('lazy')
