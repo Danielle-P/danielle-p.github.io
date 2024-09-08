380205af8dfdc85816a032a9f313197641b91919
Danielle Parkinson <dparkinson2@hotmail.co.uk>
2024-09-08 22:30:13 +0200

    menu updates and fixes

--- scripts/nav.js
+++ scripts/nav.js
@@ -6,26 +6,17 @@
   var navButton = getElement('btn-menu')
   var navPanel = getElement('nav-panel')
   var menuIcon = getElement('icon-container')
-  var mainContent = document.getElementsByTagName('main')[0]
 
   if (navButton) {
     navButton.addEventListener('click', function () {
       if (navPanel.classList.contains('hide')) {
         navPanel.classList.remove('hide')
-        menuIcon.classList.remove('open')
+        menuIcon.classList.add('open')
       } else {
         navPanel.classList.add('hide')
-        menuIcon.classList.add('open')
+        menuIcon.classList.remove('open')
       }
     })
 
-    if (mainContent) {
-      mainContent.addEventListener('click', function () {
-        if (navPanel.classList.contains('hide')) {
-          navPanel.classList.remove('hide')
-          menuIcon.classList.remove('open')
-        }
-      })
-    }
   }
 })()
