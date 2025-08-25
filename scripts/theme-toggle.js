31a3eecac6d8921d4b6b7550b2338e0729124f66
Danielle-P <dparkinson2@hotmail.co.uk>
2025-08-25 23:13:33 +0200

    Update color switch js

--- scripts/theme-toggle.js
+++ scripts/theme-toggle.js
@@ -1,23 +1,35 @@
-;(function() {
-  var themeToggle = document.getElementById('theme-toggle')
+(function () {
+  const themeToggle = document.querySelector('.theme-toggle-switch')
+  const rootElement = document.documentElement
 
-  if (localStorage.getItem('theme') === 'light') {
-    document.body.setAttribute('data-theme', 'light')
-    if (themeToggle) {
-      themeToggle.checked = true
-    }
+  function getSystemTheme() {
+    return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+  }
+
+  function setTheme(theme) {
+    rootElement.setAttribute('data-theme', theme)
+    if (themeToggle) themeToggle.checked = theme === 'dark'
+  }
+
+  function showThemeToggle() {
+    if (themeToggle) themeToggle.style.display = 'inline-block'
+  }
+
+  function initTheme() {
+    let theme = getSystemTheme()
+    setTheme(theme)
+    showThemeToggle()
   }
 
   if (themeToggle) {
-    themeToggle.addEventListener('change', function(themeChange) {
-      document.body.setAttribute('data-theme-animate', '')
-      if (themeChange.target.checked) {
-        document.body.setAttribute('data-theme', 'light')
-        localStorage.setItem('theme', 'light')
-      } else {
-        document.body.setAttribute('data-theme', 'dark')
-        localStorage.removeItem('theme')
-      }
+    themeToggle.addEventListener('change', function () {
+      setTheme(this.checked ? 'dark' : 'light')
     })
   }
+
+  window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
+    setTheme(e.matches ? 'dark' : 'light')
+  })
+
+  initTheme()
 })()
